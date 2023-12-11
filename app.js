5c15b73a4899985b8b75df0637efc9eb54b66e3d
FFFiend <owais.zahid@mail.utoronto.ca>
2023-12-11 17:43:14 -0500

    added post method for user creation

--- app.js
+++ app.js
@@ -2,22 +2,28 @@ const PORT = process.env.PORT || 8000;
 const express = require("express");
 const app = express();
 
+const user = require("models/User")
+
 app.use(express.json());
 
-
+// for template HTMLs
 app.set('view engine','ejs')
 
 // what does this line do?
+// uses described folder for static resources
 app.use(express.static('./public'))
 
+// listening to port
 app.listen(PORT, () => {
     console.log('Server is listening on port:', PORT);
 })
 
+// main page, lol
 app.get('/', (req, res)=>{
     res.send('Welcome')
 });
 
+// messing around in routes here
 app.get('/test', (req, res)=>{
     res.send("oh hi")
 });
@@ -26,11 +32,12 @@ app.post('/post', (req, res) =>{
     res.send('POSt method')
 });
 
-// parameter 
+// parameter passing thru URL
 app.get('/test/:username/:password', (req, res)=>{
     res.render("index", {name:"Owais"})
 });
 
+// testing profile creation ejs  
 app.get('/profile', (req, res)=>{
     res.render("profile")
 })
@@ -39,7 +46,13 @@ app.delete('/del', (req, res)=>{
     res.send('Delete method')
 });
 
-app.put("/createuser/:",(req, res)=>{
-    res.body.name;
+app.post("/create", async function (req, res){
+    // idea is collect info from DOM 
+    let this_user = await user.create({
+        username: req.body.username,
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+        age: req.body.age
+    });
 })
-
